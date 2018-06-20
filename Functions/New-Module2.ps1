e2ef7e5bd752f3d416810a4477a4fb3a13bb6879
danblank000 <danblank000@gmail.com>
2018-06-20 16:46:12 +0100

    Added New-Module2.ps1 to Functions. Created DanBlank module.

---
+++ Functions/New-Module2.ps1
@@ -0,0 +1,36 @@
+﻿Function New-Module2
+{
+
+    [cmdletbinding()]
+    param(
+    [Parameter(Mandatory=$true, Position=0)] 
+    [string]$ModuleName,
+      
+    [Parameter(Position=1)] 
+    [string]$Description
+    )
+    
+        # We cloned our project to C:\sc\PSStackExchange
+        $Path = 'C:\Stuff\git\Powershell\Modules'
+        $Author = 'Dan Blank'
+            
+        # Create the module and private function directories
+        mkdir $Path\$ModuleName
+        mkdir $Path\$ModuleName\Private
+        mkdir $Path\$ModuleName\Public
+        mkdir $Path\$ModuleName\en-GB # For about_Help files
+            
+        #Create the module and related files
+        New-Item "$Path\$ModuleName\$ModuleName.psm1" -ItemType File
+        New-Item "$Path\$ModuleName\$ModuleName.Format.ps1xml" -ItemType File
+        New-Item "$Path\$ModuleName\en-GB\about_$ModuleName.help.txt" -ItemType File
+        $PSVersion = [string]$PSVersionTable.PSVersion.Major + "." + [string]$PSVersionTable.PSVersion.Minor
+        New-ModuleManifest -Path $Path\$ModuleName\$ModuleName.psd1 `
+                            -RootModule $ModuleName.psm1 `
+                            -Description $Description `
+                            -PowerShellVersion $PSVersion `
+                            -Author $Author `
+                            -FormatsToProcess "$ModuleName.Format.ps1xml"
+
+        # Copy the public/exported functions into the public folder, private functions into private folder
+}
