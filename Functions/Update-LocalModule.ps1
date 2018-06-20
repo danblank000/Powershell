b8e16623d85188bfd2caad1190f7ce4795a5a283
danblank000 <danblank000@gmail.com>
2018-06-20 17:56:35 +0100

    Added Update-LocalModule.ps1 to Functions

---
+++ Functions/Update-LocalModule.ps1
@@ -0,0 +1,28 @@
+﻿Function Update-LocalModule
+{
+    
+    $LiveLocation = Get-ChildItem "\\kslnas01\folderredirection$\$env:USERNAME\My Documents\WindowsPowerShell\Modules\"
+    $GitLocation = Get-ChildItem "C:\Stuff\Git\Powershell\Modules\"
+    
+        foreach($GitMod in $GitLocation)
+        {
+        
+            foreach($LiveMod in $LiveLocation)
+            {
+
+                if($GitMod.name -match $LiveMod.name)
+                {
+
+                Write-Host "yes $LiveMod and $GitMod"
+
+                }
+
+                else
+                {
+                Write-Host "No $LiveMod and $GitMod"
+                }
+
+            }
+
+        }
+}
